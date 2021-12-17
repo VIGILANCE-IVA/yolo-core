937324628d5bb0e69b78165276898f2d380420be
Paul Jeremiah Mugaya <35077129+paulgrammer@users.noreply.github.com>
2021-12-17 23:29:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.1.0',
     description='Python wrapper for YOLO Real-Time ObjectDetection Library',
     author='Paul Jeremiah Mugaya',
-    install_requires=['tensorflow', 'pytesseract', 'easydict', 'opencv-python==4.1.1.26'],
+    install_requires=['tensorflow-gpu', 'pytesseract', 'easydict', 'opencv-python==4.1.1.26'],
     license='MIT',
     data_files=[('classes', ['data/classes/coco.names'])]
 )
